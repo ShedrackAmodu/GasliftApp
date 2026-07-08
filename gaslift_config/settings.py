e35b6f6383e8e32bc90143cf0ab01786892f861a
ShedrackAmodu <147725659+ShedrackAmodu@users.noreply.github.com>
2026-07-08 18:34:15 +0100

    Remove unused Django REST Framework; app uses JsonResponse/HttpResponse only

--- gaslift_config/settings.py
+++ gaslift_config/settings.py
@@ -25,7 +25,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     # Third-party apps
-    'rest_framework',
     'corsheaders',
     
     # Local apps
@@ -127,12 +126,6 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
     "https://Amodu.pythonanywhere.com",
 ]
-
-# REST Framework settings
-REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 50
-}
 
 # Logging configuration
 LOGGING = {
